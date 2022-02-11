9d2d1718f65dcb23b05f446a47232d5720217ac7
Michael Good <mjgood@coastal.edu>
2022-02-10 23:41:20 -0500

    adding

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace PO_Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Happy Birthday";
+            test = swapCase(test);
+            Console.WriteLine(test);
+
+            test = "MANY THANKS";
+            test = swapCase(test);
+            Console.WriteLine(test);
+
+            test = "sPoNtAnEoUs";
+            test = swapCase(test);
+            Console.WriteLine(test);
+        }
+
+
+        public static string swapCase(string s)
+        {
+            string swap = new string(
+                s.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+            return swap;
+        }
+    }
+}
